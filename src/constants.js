2c0d24daaf95fc6a6a5a501778c4c85e184bb2f3
MyagmarsurenMike <mg.pro0314@gmail.com>
2025-05-26 19:00:32 +0800

    ec2s

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = 'http://localhost:8000'; // If using proxy
+export const BASE_URL = 'http://51.21.1.50:8000'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
